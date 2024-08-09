103fdcdf5ef3e950c91e13184c98daaaa39a7ec7
ValeriSt <valst1@abv.bg>
2024-08-09 11:45:55 +0300

    Added password token functionality and did some refactoring

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -4,15 +4,16 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController(DataContext context) : BaseApiController
+public class AccountController(DataContext context, ITokenService tokenService) : BaseApiController
 {
     [HttpPost("register")] //account/register
-    public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDto)
+    public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDto)
     {
         if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
@@ -28,11 +29,14 @@ public class AccountController(DataContext context) : BaseApiController
         context.Users.Add(user);
         await context.SaveChangesAsync();
 
-        return user;
+        return new UserDTO{
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDTO loginDto){
+    public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDto){
         var user = await context.Users.FirstOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
 
         if (user == null) return Unauthorized("Invalid username");
@@ -48,7 +52,10 @@ public class AccountController(DataContext context) : BaseApiController
                 return Unauthorized("Invalid password");
             }
         }
-        return user;
+        return new UserDTO{
+            Username = user.UserName,
+            Token = tokenService.CreateToken(user)
+        };
     }
     
     private async Task<bool> UserExists(string username){
--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ namespace API.Controllers;
 
 public class UsersController(DataContext context) : BaseApiController
 {
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -16,6 +18,7 @@ public class UsersController(DataContext context) : BaseApiController
         return users;
     }
 
+    [Authorize]
     [HttpGet("{id:int}")] // api/users/id
     public async Task<ActionResult<AppUser>> GetUsers(int id)
     {
