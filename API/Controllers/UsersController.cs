c58c549dad6f88fbf9548ae63a36e3a7ddb23630
ValeriSt <valst1@abv.bg>
2024-08-14 11:52:29 +0300

    Added new architecture with repository pattern and integrated AutoMapper for object mapping

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,28 +1,29 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-
-public class UsersController(DataContext context) : BaseApiController
+[Authorize]
+public class UsersController(IUserRepository userRepository) : BaseApiController
 {
-    [AllowAnonymous]
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    public async Task<ActionResult<IEnumerable<MemberDTO>>> GetUsers()
     {
-        var users = await context.Users.ToListAsync();
+        var users = await userRepository.GetMembersAsync();
 
-        return users;
+        return Ok(users);
     }
 
-    [Authorize]
-    [HttpGet("{id:int}")] // api/users/id
-    public async Task<ActionResult<AppUser>> GetUsers(int id)
+    [HttpGet("{username}")] // api/users/username
+    public async Task<ActionResult<MemberDTO>> GetUsers(string username)
     {
-        var user = await context.Users.FindAsync(id);
+        var user = await userRepository.GetMemberAsync(username);
         
         if (user == null) return NotFound();
 
